d1154189c8431b2a4e742868f3fb552ded86e4eb
Wambita <wambitafana@gmail.com>
2026-03-12 21:39:09 +0300

    init: create unit test structure and configure project imports

---
+++ tests/test_image_inspector.py
@@ -0,0 +1,12 @@
+"""
+Unit tests for Image Inspector
+"""
+
+import unittest
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../src'))
+
+if __name__ == '__main__':
+    unittest.main()
