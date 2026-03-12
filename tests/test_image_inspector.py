e28c0d541a2d861d2fa2f1790568ca5d70d51e5a
Wambita <wambitafana@gmail.com>
2026-03-12 21:39:42 +0300

    feat: add metadata extractor imports and test class

--- tests/test_image_inspector.py
+++ tests/test_image_inspector.py
@@ -8,5 +8,11 @@ import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../src'))
 
+from modules.metadata_extractor import extract_metadata, convert_to_degrees
+
+
+class TestMetadataExtractor(unittest.TestCase):
+    """Test metadata extraction functionality"""
+
 if __name__ == '__main__':
     unittest.main()
